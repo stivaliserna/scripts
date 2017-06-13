38481d8a12c8a8dee931a291b03c57b1fd709973
Stivali Serna <stivaliserna08@gmail.com>
2017-06-13 17:32:56 -0400

    Add default image constant

--- species-explorer/app/app.config.js
+++ species-explorer/app/app.config.js
@@ -3,3 +3,4 @@
 angular
   .module('speciesApp')
   .constant('API_BASE_URL', 'https://cors-anywhere.herokuapp.com/http://environment.ehp.qld.gov.au/species/')
+  .constant('defaultURL', 'https://pbs.twimg.com/profile_images/600060188872155136/st4Sp6Aw.jpg')
